ccc48edc3ae5808f47a8a77283426b749ef79652
spradeep952 <spradeep952@gmail.com>
2019-05-18 11:59:39 +0530

    added all files

---
+++ may13.py
@@ -0,0 +1,205 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('hello wold')
+hello wold
+>>> print(dir(bultins)
+	  print
+	  
+SyntaxError: invalid syntax
+>>> import biltins
+	  
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    import biltins
+ModuleNotFoundError: No module named 'biltins'
+>>> import builtins
+	  
+>>> print(dir(builtin)
+
+
+
+
+	  dir(keywoeds
+	      
+SyntaxError: invalid syntax
+>>> a=input('enter name')
+	      
+enter namePradeep Sharma
+
+>>> print(a)
+	      
+Pradeep Sharma
+>>> help(print)
+	      
+Help on built-in function print in module builtins:
+
+print(...)
+    print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+    
+    Prints the values to a stream, or to sys.stdout by default.
+    Optional keyword arguments:
+    file:  a file-like object (stream); defaults to the current sys.stdout.
+    sep:   string inserted between values, default a space.
+    end:   string appended after the last value, default a newline.
+    flush: whether to forcibly flush the stream.
+
+>>> print('hello','world' sep='   ')
+	      
+SyntaxError: invalid syntax
+>>> print('hello')
+	      
+hello
+>>> print('hi')
+	      
+hi
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+hello
+Pradeep
+Sharma
+>>> print('hello','world',sep='123')
+	      
+hello123world
+>>> print('hello world',file=open(output.txt,'w')
+
+
+	vfsfsff
+	  
+SyntaxError: invalid syntax
+>>> print('hello word',file=open(output.txt,w)
+	e\
+	  
+SyntaxError: invalid syntax
+>>>  help(input)
+	  
+SyntaxError: unexpected indent
+>>> help(input)
+	  
+Help on built-in function input in module builtins:
+
+input(prompt=None, /)
+    Read a string from standard input.  The trailing newline is stripped.
+    
+    The prompt string, if given, is printed to standard output without a
+    trailing newline before reading input.
+    
+    If the user hits EOF (*nix: Ctrl-D, Windows: Ctrl-Z+Return), raise EOFError.
+    On *nix systems, readline is used if available.
+
+>>> y=print('pradeep')
+	  
+pradeep
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+P
+r
+a
+d
+e
+e
+p
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+Q
+s
+b
+e
+f
+f
+q
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+Q
+s
+b
+e
+f
+f
+q
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+enter a choice1
+1 for encryption
+ 2 fo decryption
+E
+g
+V
+Y
+Z
+Z
+e
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+Available choices
+	1 for encryption
+	2 fo decryption
+enter a choice2
+E
+g
+V
+Y
+Z
+Z
+e
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+Available choices
+	1 for encryption
+	2 fo decryption
+enter a choice1
+O
+q
+`
+c
+d
+d
+o
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+Available choices
+	1 for encryption
+	2 fo decryption
+enter a choice2
+O
+q
+`
+c
+d
+d
+o
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+Available choices
+	1 for encryption
+	2 fo decryption
+enter a choice1
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+Available choices
+	1 for encryption
+	2 fo decryption
+enter a choice1
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+Available choices
+	1 for encryption
+	2 fo decryption
+enter a choice1
+>>> 
+================= RESTART: C:/Users/Pradeep Sharma/Desktop/New folder/example.py =================
+enter namePradeep
+Available choices
+	1 for encryption
+	2 fo decryption
+enter a choice1
+>>>print("hello") 
